91757eda919d7b082bf4518cee8cb965236d0c26
Nova <Johnny@mottl.us>
2018-10-26 20:37:25 -0400

    Finalized mock up for SearchBar

--- ravemous/src/App.js
+++ ravemous/src/App.js
@@ -25,11 +25,15 @@ const businesses = [
 ];
 
 class App extends React.Component {
-  render() {
+    searchYelp(term, location, sortBy) {
+        console.log('Search Yelp with pizza, Brooklyn, best_match');
+    } 
+
+    render() {
       return (
         <div className="App">
             <h1>ravenous</h1>
-            <SearchBar />
+              <SearchBar serachYelp={this.searchYelp} />
               <BusinessList businesses={businesses} />
         </div>
       );
